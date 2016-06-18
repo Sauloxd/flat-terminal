fee205857fe5fc2e54c53c5cc82dad2dbadb355f
Adriano Valente <dri9595@gmail.com>
2016-06-18 01:57:36 -0300

    moved typing script to myscript.js

--- app/pages/terminal/myscript.js
+++ app/pages/terminal/myscript.js
@@ -1,1 +1,12 @@
 console.log('loaded only here! :)')
+console.log('hello saulo!');
+$(function(){
+    $(".terminal-title").typed({
+      strings: [
+        "$ Operational Systems",
+        "Process scheduling"
+      ],
+      typeSpeed: 0,
+      backDelay: 2000
+    });
+});
