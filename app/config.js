2060243c9aa53b9555299d0111aaafb68b049341
Sauloxd <saulotoshi@gmail.com>
2016-06-18 01:03:21 -0300

    computer screen and terminal merged.

--- app/config.js
+++ app/config.js
@@ -3,30 +3,13 @@ angular
   .config(function($stateProvider, $urlRouterProvider) {
   //
   // For any unmatched url, redirect to /state1
-  $urlRouterProvider.otherwise("/state1");
+  $urlRouterProvider.otherwise("/terminal");
   //
   // Now set up the states
   $stateProvider
-    .state('state1', {
-      url: "/state1",
-      templateUrl: "partials/state1.html"
-    })
-    .state('state1.list', {
-      url: "/list",
-      templateUrl: "partials/state1.list.html",
-      controller: function($scope) {
-        $scope.items = ["A", "List", "Of", "Items"];
-      }
-    })
-    .state('state2', {
-      url: "/state2",
-      templateUrl: "partials/state2.html"
-    })
-    .state('state2.list', {
-      url: "/list",
-      templateUrl: "partials/state2.list.html",
-      controller: function($scope) {
-        $scope.things = ["A", "Set", "Of", "Things"];
-      }
+    .state('terminal', {
+      url: "/terminal",
+      templateUrl: "pages/terminal/terminal.html"
     });
+
 });
